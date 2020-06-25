379c8763bc5b064d56b54ce390c2f8e3494a61a8
Viktor Balobanov <vikbalob94@gmail.com>
2020-06-25 17:07:53 +0400

    new_file

---
+++ Lesson1/Symbols.java
@@ -0,0 +1,8 @@
+public class Symbols {
+    public static void main(String[] args) {
+        char symbol;
+        for (symbol = 33; symbol <= 126; symbol++) {
+            System.out.println(symbol);
+        }
+    }
+}
