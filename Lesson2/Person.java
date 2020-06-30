4257ff5f26188633ba1ec9662d0e9502c65b91fa
Viktor Balobanov <vikbalob94@gmail.com>
2020-06-30 18:56:03 +0400

    new_file

---
+++ Lesson2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String sex = male;
+    String name = Paul;
+    int height = 180;
+    int weight = 80;
+    int age = 30;
+
+    boolean go() {
+
+    }
+
+    void sit() {
+
+    }
+
+    String learnjava() {
+
+    }
+
+    boolean run() {
+
+    }
+
+    void talk() {
+        
+    }
+}
