6fb31189098e3e43593633544422a6a68e5afffa
Viktor Balobanov <vikbalob94@gmail.com>
2020-07-01 19:43:28 +0400

    new_file

---
+++ Lesson2/Wolf.java
@@ -0,0 +1,31 @@
+public class Wolf {
+	
+	String sex = "male";
+	String name = "Wild";
+	float weight = 40.0f;
+	int age = 12; 
+	String color = "grey";
+
+	boolean go() {
+		return true;
+	}
+
+	boolean sit() {
+		return true;
+	}
+
+	String run() {
+		return "I`m running!";
+	}
+
+	String howl() {
+		return "Auuu!!";
+	}
+
+	String hunt() {
+		return "Looking for prey";
+	}
+
+
+
+}
---
+++ Lesson2/WolfTest.java
@@ -0,0 +1,12 @@
+public class WolfTest {
+
+	public static void main (String [] args) {
+		Wolf wolfOne = new Wolf();
+
+		System.out.println(wolfOne.name);
+		System.out.println(wolfOne.run());
+		System.out.println(wolfOne.go());
+		System.out.println(wolfOne.howl());
+
+	}
+}
