fec3ad99eec5ea0d3df098c1b51effa8c95775b3
Viktor Balobanov <vikbalob94@gmail.com>
2020-07-01 17:26:19 +0400

    update3

--- Lesson2/Person.java
+++ Lesson2/Person.java
@@ -2,7 +2,7 @@ public class Person {
     String sex = "male";
     String name = "Paul";
     int height = 180;
-    float weight = 80.0;
+    float weight = 80.0f;
     int age = 30;
 
     boolean go() {
