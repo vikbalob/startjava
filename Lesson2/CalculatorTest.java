dd88207fcf34675360b0ff5a409267d26c91fe28
Viktor Balobanov <vikbalob94@gmail.com>
2020-07-09 18:44:09 +0400

    calc_update4

--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -5,29 +5,29 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         Scanner sc = new Scanner(System.in);
+        String answer = "Y";
 
-        do {
-            System.out.println("Enter first number");
-            calc.setNum1(sc.nextFloat());
-            System.out.println("Enter char of the math operation");
-            calc.setOperation(sc.next().charAt(0));
-            System.out.println("Enter second number");
-            calc.setNum2(sc.nextFloat());
-            System.out.println(calc.calculate());
-            System.out.println("Do you want to continue? [Y/N]:");
-            sc.nextLine();
-            String answer = sc.nextLine();
-            if (answer == "Y") {
-                break;
-            } else if (answer == "N") {
-                break;
-            } else {
-                do {
-                    System.out.println("Enter the correct answer! [Y/N]");
-                    sc.nextLine();
-                } while (sc.nextLine() != "Y" || sc.nextLine() != "N");
+        while (!answer.equals("N")) {
+            if (answer.equals("Y")) {
+                System.out.println("Enter first number");
+                calc.setNum1(sc.nextFloat());
+                System.out.println("Enter char of the math operation");
+                calc.setOperation(sc.next().charAt(0));
+                System.out.println("Enter second number");
+                calc.setNum2(sc.nextFloat());
+                System.out.println(calc.calculate());
             }
-        } while (sc.nextLine().equalsIgnoreCase("Y"));
+            System.out.println("Do you want to continue? [Y/N]:");
+            answer = sc.next();
+            if (!answer.equals("N") && !answer.equals("Y")) {
+                System.out.println("Wrong answer! Try again!");
+            }
+        }
     }
 }
+
+
+
+
+
 
