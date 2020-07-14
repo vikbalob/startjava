09705c0a9a261c9d773ad72145c127883a53f386
Viktor Balobanov <vikbalob94@gmail.com>
2020-07-15 00:04:40 +0400

    jaeger_update3

--- Lesson2/JaegerTest.java
+++ Lesson2/JaegerTest.java
@@ -2,25 +2,25 @@ public class JaegerTest {
 
     public static void main(String[] args) {
         Jaeger jaeger1 = new Jaeger("Crimson Typhoon", "Mark-4", "China", 76.2f, 1722f, 9, 8, 6);
-        System.out.println("ModelName: " + jaeger1.modelName);
-        System.out.println("Mark: " + jaeger1.mark);
-        System.out.println("Origin: " + jaeger1.origin);
-        System.out.println("Height: " + jaeger1.height);
-        System.out.println("Weight: " + jaeger1.weight);
-        System.out.println("Speed: " + jaeger1.speed);
-        System.out.println("Strength: " + jaeger1.strength);
-        System.out.println("Armor: " + jaeger1.armor);
+        System.out.println("ModelName: " + jaeger1.getModelName());
+        System.out.println("Mark: " + jaeger1.getMark());
+        System.out.println("Origin: " + jaeger1.getOrigin());
+        System.out.println("Height: " + jaeger1.getHeight());
+        System.out.println("Weight: " + jaeger1.getWeight());
+        System.out.println("Speed: " + jaeger1.getSpeed());
+        System.out.println("Strength: " + jaeger1.getStrength());
+        System.out.println("Armor: " + jaeger1.getArmor());
 
         System.out.println();
 
         Jaeger jaeger2 = new Jaeger("Horizon Brave", "Mark-1", "China", 72.54f, 7890f, 8, 4, 6);
-        System.out.println("ModelName: " + jaeger2.modelName);
-        System.out.println("Mark: " + jaeger2.mark);
-        System.out.println("Origin: " + jaeger2.origin);
-        System.out.println("Height: " + jaeger2.height);
-        System.out.println("Weight: " + jaeger2.weight);
-        System.out.println("Speed: " + jaeger2.speed);
-        System.out.println("Strength: " + jaeger2.strength);
-        System.out.println("Armor: " + jaeger2.armor);
+        System.out.println("ModelName: " + jaeger2.getModelName());
+        System.out.println("Mark: " + jaeger2.getMark());
+        System.out.println("Origin: " + jaeger2.getOrigin());
+        System.out.println("Height: " + jaeger2.getHeight());
+        System.out.println("Weight: " + jaeger2.getWeight());
+        System.out.println("Speed: " + jaeger2.getSpeed());
+        System.out.println("Strength: " + jaeger2.getStrength());
+        System.out.println("Armor: " + jaeger2.getArmor());
     }
 }
