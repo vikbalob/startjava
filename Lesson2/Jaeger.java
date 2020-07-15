eba9e7b4b28c2970f78acc2e249cd27b787e80f1
Viktor Balobanov <vikbalob94@gmail.com>
2020-07-15 15:46:39 +0400

    jaeger_final

--- Lesson2/Jaeger.java
+++ Lesson2/Jaeger.java
@@ -9,6 +9,16 @@ public class Jaeger {
     private int strength;
     private int armor;
 
+    public Jaeger(String modelName, String mark, String origin, float height, float weight, int speed, int strength, int armor) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.origin = origin;
+        this.height = height;
+        this.weight = weight;
+        this.speed = speed;
+        this.strength = strength;
+        this.armor = armor;
+    }
     public String getModelName() {
         return modelName;
     }
@@ -70,17 +80,6 @@ public class Jaeger {
     }
 
     public void setArmor(int armor) {
-        this.armor = armor;
-    }
-
-    public Jaeger(String modelName, String mark, String origin, float height, float weight, int speed, int strength, int armor) {
-        this.modelName = modelName;
-        this.mark = mark;
-        this.origin = origin;
-        this.height = height;
-        this.weight = weight;
-        this.speed = speed;
-        this.strength = strength;
         this.armor = armor;
     }
 }
