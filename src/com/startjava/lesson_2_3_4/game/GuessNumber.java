c376b7d561aa9776266e8abcc540e8ef4fb00011
Viktor Balobanov <vikbalob94@gmail.com>
2020-09-16 23:21:03 +0400

    Refactoring code

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -1,42 +1,74 @@
 package com.startjava.lesson_2_3_4.game;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class GuessNumber {
-
     Scanner sc = new Scanner(System.in);
-    private Player player1;
-    private Player player2;
+    private final Player player1;
+    private final Player player2;
+    private int randomNum;
+
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
     }
 
-    public void startGame() {
-        int compNum = (int) (Math.random() * 101);
-        do {
-            System.out.println("Enter the number (" + player1.getName() + "):");
-            player1.setNumber(sc.nextInt());
-            if (player1.getNumber() < compNum) {
-                System.out.println("The number is greater than yours!");
-            } else if (player1.getNumber() > compNum) {
-                System.out.println("The number less than yours!");
-            } else if (player1.getNumber() == compNum) {
-                System.out.println("Winner is " + player1.getName());
-                break;
-            }
+    private void numbersOfPlayer(Player player) {
+        System.out.println("Numbers of " + player.getName());
+        for (int number : player.getNumbers()) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+    }
 
-            System.out.println("Enter the number (" + player2.getName() + "):");
-            player2.setNumber(sc.nextInt());
-            if (player2.getNumber() < compNum) {
-                System.out.println("The number is greater than yours!");
-            } else if (player2.getNumber() > compNum) {
-                System.out.println("The number less than yours!");
-            } else if (player2.getNumber() == compNum) {
-                System.out.println("Winner is " + player2.getName());
-                break;
+    private void checkNumber(Player player) {
+        System.out.print(player.getName() + ", enter the number: ");
+        player.setNumbers(sc.nextInt());
+        if (player.getNumbers()[player.getAttempt()] < randomNum) {
+            System.out.println("The number is greater than yours!");
+        } else if (player.getNumbers()[player.getAttempt()] > randomNum) {
+            System.out.println("The number less than yours!");
+        } else if (player.getNumbers()[player.getAttempt()] == randomNum) {
+            System.out.println();
+            System.out.println(player.getName() + " guessed the number " + randomNum +
+                    " on " + player.getAttempt() + " attempts.");
+        }
+    }
+
+    private void addAttempt(Player player) {
+        player.setAttempt(player.getAttempt() + 1);
+    }
+
+    public boolean checkEquals(Player player) {
+        if (player.getNumbers().equals(randomNum)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void startGame() {
+        randomNum = (int) (Math.random() * 101);
+        do {
+            while (player1.getAttempt() < 10 || player2.getAttempt() < 10) {
+                addAttempt(player1);
+                checkNumber(player1);
+
+                if (!checkEquals(player1)) {
+                    addAttempt(player2);
+                    checkNumber(player2);
+                }
             }
-        } while (true);
+        } while (!checkEquals(player1) || !checkEquals(player2));
+
+        if (player1.getAttempt() == 10 || player2.getAttempt() == 10) {
+            System.out.println("Attempts is ended.");
+            System.out.println("Computer`s number was " + randomNum);
+        }
+        numbersOfPlayer(player1);
+        numbersOfPlayer(player2);
+        Arrays.fill(player1.getNumbers(), 0, player1.getAttempt(), 0);
+        Arrays.fill(player2.getNumbers(), 0, player2.getAttempt(), 0);
     }
 }
